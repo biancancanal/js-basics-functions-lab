95b73fac0d2937b00dfc92f0d9bed68ebbf35420
Bianca <biancancanal@gmail.com>
2021-03-18 09:46:48 -0500

    commit tree f453d28dc49c5c31fc66b84ac33af082bba1f547

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+let num1 = 42
+let start
+let destination
+
+function distanceFromHqInBlocks(block) {
+    return Math.abs(num1 - block) 
+}
+
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block)*264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)*264
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0  
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400)*.02 
+    } else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
